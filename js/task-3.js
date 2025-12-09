b0f98c3fffe5eda256cea95d53a409b6283c33e3
mephisto <lukyanpopovish@gmail.com>
2025-12-09 09:57:59 +0200

    made hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,24 @@
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-}
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+  
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
+    },
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+};
 
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
